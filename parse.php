56a6711ef69f96f0b710f121dac6355339bb60bd
Matúš Ďurica <mtsdurica@gmail.com>
2023-02-10 19:27:29 +0100

    pain continues

--- parse.php
+++ parse.php
@@ -18,6 +18,15 @@ include "instr_2_arg.php";
 
 ini_set("display_errors", "stderr");
 
+function remove_comment($str)
+{
+    if (strpos($str, "#")) {
+        return $str = strstr($str, "#", true);
+    } else {
+        return $str;
+    }
+}
+
 $header_flag = false;
 $instr_cnt = 1;
 $parser = new Parse_visitor();
@@ -73,6 +82,7 @@ while ($stream = fgets(STDIN)) {
                     $split_str[1]
                 ):
                     $arg = $split_str[1];
+                    $arg = remove_comment($arg);
                     $arg_type = "var";
                     break;
                 case (bool) preg_match("/^(string)@*/", $split_str[1]):
@@ -80,13 +90,16 @@ while ($stream = fgets(STDIN)) {
                         $split_str[1],
                         strstr($split_str[1], "@", true) . "@"
                     );
+                    $arg = remove_comment($arg);
                     $arg_type = strstr($split_str[1], "@", true);
                     break;
+                //TODO: support for octal and hexa format
                 case (bool) preg_match("/^(int)@[0-9]*/", $split_str[1]):
                     $arg = trim(
                         $split_str[1],
                         strstr($split_str[1], "@", true) . "@"
                     );
+                    $arg = remove_comment($arg);
                     $arg_type = strstr($split_str[1], "@", true);
                     break;
                 case (bool) preg_match("/^(bool)@[true|false]/", $split_str[1]):
@@ -94,7 +107,7 @@ while ($stream = fgets(STDIN)) {
                         $split_str[1],
                         strstr($split_str[1], "@", true) . "@"
                     );
-                    $arg = strstr($arg, "#", true);
+                    $arg = remove_comment($arg);
                     $arg_type = strstr($split_str[1], "@", true);
                     break;
                 default:
@@ -122,9 +135,12 @@ while ($stream = fgets(STDIN)) {
                 $arg1 = $split_str[1];
                 $arg1_type = "var";
             }
-            // pridat moznost ze koment je debil
-            if (preg_match("/^(int|string|bool)$/", $split_str[2])) {
-                $arg2 = $split_str[2];
+            if (preg_match("/^(int|string|bool)/", $split_str[2])) {
+                $arg2 = trim(
+                    $split_str[2],
+                    strstr($split_str[2], "@", true) . "@"
+                );
+                $arg2 = remove_comment($arg2);
                 $arg2_type = "type";
             }
             $obj = new Instr_2_arg(
